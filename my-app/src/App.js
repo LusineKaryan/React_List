e01153b39c97c8b8a3dce8307580e81f8f1dd1b7
LusineKaryan <72447443+LusineKaryan@users.noreply.github.com>
2020-12-10 23:07:12 +0100

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './App.css';
+import ToDoItem from './ToDoItem/ToDoItem.js';
+import todosData from './todosData.js';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todoItems : todosData
+    }
+  }
+  handleChange = id =>{
+    const index = this.state.todoItems.map(item =>item.id).indexOf(id);
+    this.setState(state => {
+      let{todoItems} = state;
+      todoItems[index].completed = true;
+      return todoItems;
+    })
+  }
+  render(){
+    const{todoItems} = this.state;
+    const activeTasks = todoItems.filter(task => task.completed === false);
+    const completedTasks = todoItems.filter(task => task.completed === true);
+    const finalTasks = [...activeTasks,...completedTasks].map(item => {
+      return(
+        <ToDoItem
+          key={item.id}
+          description={item.description}
+          completed={item.completed}
+          handleChange={() => { this.handleChange(item.id) }}
+        />
+      )
+    })
+    
+    return (
+      <div className="App">
+        <h1 className="title">Des plans pour aujourd'hui</h1>
+        <h2 className="titleBlock">Tâches</h2>
+        {finalTasks}
+      </div>
+  );
+  }
+  
+}
+
+export default App;
---
+++ my-app/src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 0,
+        description: 'Programmer sur Argon',
+        completed: false
+    },
+    {
+        id: 1,
+        description: 'Faire des courses',
+        completed: false
+    },
+    {
+        id: 2,
+        description: 'Rencontrer des amis',
+        completed: false
+    },
+    {
+        id: 3,
+        description: 'Cuisiner le diner',
+        completed: false
+    },
+    {
+        id: 4,
+        description: 'Préparer des documents',
+        completed: false
+    }
+];
+
+export default todosData;
